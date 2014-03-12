fac4e37d8e7967e59dd127458097eb158aca6780
Dmitry Shatov <d.shatty@gmail.com>
2014-03-12 14:56:12 +0700

    Added drawObjects

--- src/main.lua
+++ src/main.lua
@@ -1,5 +1,25 @@
 function love.load()
+	Grid = {}
+	for i = 0, 2 do
+		Grid[i] = {}
+	end
 
+	Grid[0][0] = 'x'
+	Grid[1][1] = 'x'
+	Grid[2][1] = 'o'
+end
+
+function drawObjects(x_offset, y_offset, zoom)
+	for x = 0, 2 do
+		for y = 0, 2 do
+			if Grid[x][y] == 'x' then
+				love.graphics.line(x_offset + x * zoom, y_offset + y * zoom, x_offset + (x + 1) * zoom, y_offset + (y + 1) * zoom)
+				love.graphics.line(x_offset + (x + 1) * zoom, y_offset + y * zoom, x_offset + x * zoom, y_offset + (y + 1) * zoom)
+			elseif Grid[x][y] == 'o' then
+				love.graphics.circle("line", x_offset + (x + 0.5) * zoom, y_offset + (y + 0.5) * zoom, zoom / 2, 100)
+			end
+		end
+	end
 end
 
 function drawGrid(x_offset, y_offset, zoom, width, height)
@@ -14,6 +34,9 @@ end
 function love.draw()
 	love.graphics.setColor(128, 0, 0, 255)
 	drawGrid(16, 16, 128, 3, 3)
+
+	love.graphics.setColor(0, 128, 0, 255)
+	drawObjects(16, 16, 128)
 	love.graphics.setColor(255, 255, 255, 255)
 	love.graphics.print('Hello World!', 400, 300)
 end
