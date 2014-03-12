fefcd51f7de081bdd0ad15b598813c41e919fb50
Dmitry Shatov <d.shatty@gmail.com>
2014-03-12 14:28:04 +0700

    Added drawGrid

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,19 @@
+function love.load()
+
+end
+
+function drawGrid(x_offset, y_offset, zoom, width, height)
+	for i = 0, width do
+		love.graphics.line(x_offset + i * zoom, y_offset, x_offset + i * zoom, y_offset + zoom * width)
+	end
+	for i = 0, height do
+		love.graphics.line(x_offset, y_offset + i * zoom, x_offset + zoom * height, y_offset + i * zoom)
+	end
+end
+
 function love.draw()
-    love.graphics.print('Hello World!', 400, 300)
+	love.graphics.setColor(128, 0, 0, 255)
+	drawGrid(16, 16, 128, 3, 3)
+	love.graphics.setColor(255, 255, 255, 255)
+	love.graphics.print('Hello World!', 400, 300)
 end
