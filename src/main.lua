a3d43fda534eb64dfead853d154535bf2d39de09
Dmitry Shatov <d.shatty@gmail.com>
2014-03-12 16:48:45 +0700

    Version 1

--- src/main.lua
+++ src/main.lua
@@ -1,12 +1,51 @@
 function love.load()
+	love.window.setMode(416, 424, {resizable=false, vsync=false})
+	love.window.setTitle("Heriki-oniki")
 	Grid = {}
 	for i = 0, 2 do
 		Grid[i] = {}
 	end
+	CurPlayer = 'x'
+end
 
-	Grid[0][0] = 'x'
-	Grid[1][1] = 'x'
-	Grid[2][1] = 'o'
+function checkLine(pt, dir, len)
+	if pt.x + dir.x * (len - 1) > 2 or
+			pt.x + dir.x * (len - 1) < 0 or
+			pt.y + dir.y * (len - 1) > 2 or
+			pt.y + dir.y * (len - 1) < 0 then
+		return nil
+	else
+		local owner = Grid[pt.x][pt.y]
+		for i = 1, len - 1 do
+			if Grid[pt.x + dir.x * i][pt.y + dir.y * i] ~= owner then
+				return nil
+			end
+		end
+		return owner
+	end
+end
+
+function checkEndOfGame()
+	for x = 0, 2 do
+		for y = 0, 2 do
+			for dx = -1, 1 do
+				for dy = -1, 1 do
+					if dx ~= 0 or dy ~= 0 then
+						local pt = {}
+						local dir = {}
+						pt.x = x
+						pt.y = y
+						dir.x = dx
+						dir.y = dy
+						Winner = checkLine(pt, dir, 3)
+						if Winner ~= nil then
+							return
+						end
+					end
+				end
+			end
+		end
+	end
 end
 
 function love.update(dt)
@@ -14,7 +53,7 @@ function love.update(dt)
 end
 
 function love.mousepressed(x, y, button)
-	if button == "l" then
+	if button == "l" and Winner == nil then
 		LastMouse = {}
 		LastMouse.x = x
 		LastMouse.y = y
@@ -24,8 +63,20 @@ end
 function love.mousereleased(x, y, button)
 	if button == "l" and LastMouse ~= nil then
 		if LastMouse.x == x and LastMouse.y == y then
-			print("LKM: x=" .. x .. ", y=" .. y)
+			local logic_x = math.floor((x - 16) / 128)
+			local logic_y = math.floor((y - 16) / 128)
 			LastMouse = nil
+			if logic_x >= 0 and logic_x < 3 and logic_y >= 0 and logic_y < 3 then
+				if Grid[logic_x][logic_y] == nil then
+					Grid[logic_x][logic_y] = CurPlayer
+					checkEndOfGame()
+					if CurPlayer == 'x' then
+						CurPlayer = 'o'
+					else
+						CurPlayer = 'x'
+					end
+				end
+			end
 		end
 	end
 end
@@ -59,5 +110,9 @@ function love.draw()
 	love.graphics.setColor(0, 128, 0, 255)
 	drawObjects(16, 16, 128)
 	love.graphics.setColor(255, 255, 255, 255)
-	love.graphics.print('Hello World!', 400, 300)
+	if Winner ~= nil then
+		love.graphics.print('Winner: ' .. Winner, 16, 408)
+	else
+		love.graphics.print('Current: ' .. CurPlayer, 16, 408)
+	end
 end
